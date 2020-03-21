7d8a6b765f31512ac4f686548ad5e2cf8703bda4
Nico Greger <nico.greger@tum.de>
2020-03-21 17:57:58 +0100

    Added register() Mock

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_json import FlaskJSON
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, instance_relative_config=True)
+json = FlaskJSON(app)
 app.config.from_object(__name__ + '.settings')  # default settings
 app.config.from_object('config')  # deployment specific settings (no version control)
 
--- backend/views.py
+++ backend/views.py
@@ -1,6 +1,36 @@
+from datetime import datetime
+
+from flask import request
 
 from backend import app
+from flask_json import json_response
+
+import uuid
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route("/test/")
+def testBla():
+    return json_response(time=datetime.now())
+
+
+@app.route("/register/", methods=["POST"])
+def register():
+    print(request.is_json)
+    data = request.get_json(force=True)
+    print("Hier")
+    username = data["username"]
+    password = data["password"]
+
+    user_uuid = uuid.uuid1()
+
+    # TODO: Check if already exists
+    # TODO: Save that scheissdregg
+
+    response_dict = {"user_uuid": user_uuid, "username_test_pls_remove_me": username}
+
+    return json_response(**response_dict)

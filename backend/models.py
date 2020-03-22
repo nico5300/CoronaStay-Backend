307538b045191304f0adff047a5ecf2421c09b21
Nico Greger <nico.greger@tum.de>
2020-03-22 15:33:40 +0100

    Started models.py

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,17 @@
-
 from backend import db
+
+class User(db.Model):
+    username = db.Column(db.String, primary_key=True)
+    api_key = db.Column(db.String, unique=True)
+    stories = #TODO
+
+
+class Story(db.Model):
+    story_id = db.Column(db.String, primary_key=True)
+    creator = #TODO
+    title = db.Column(db.String)
+
+class Panel(db.Model):
+    panel_id = db.Column(db.Integer, autoincrement=True)
+    file_name = db.Column(db.String, unique=True)
 

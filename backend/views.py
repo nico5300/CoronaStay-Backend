1d0e56ee6554dd8ff41e4142545fc3e739fe80b8
Butterkeks-x86 <>
2020-03-22 23:28:11 +0100

    Implement api key check decorator

--- backend/views.py
+++ backend/views.py
@@ -1,11 +1,39 @@
 from datetime import datetime
-
-from flask import request, abort
+from functools import wraps
+from flask import request, abort, g
 
 from backend import app
+from backend.models import User
 from flask_json import json_response
 
 import uuid
+
+
+def check_api_key(api_key):
+    user = User.query.filter_by(api_key=api_key).first()
+    if user is not None:
+        g.user = User
+        return True
+    else:
+        return False
+
+
+def api_key_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        data = request.get_json()
+        if data is None:
+            abort(400)
+        try:
+            api_key = data["api_key"]
+            if check_api_key(api_key):
+                g.api_key = api_key
+                return f(*args, **kwargs)
+            else:
+                abort(401)
+        except KeyError:
+            abort(400)
+    return decorated_function
 
 
 @app.route('/')

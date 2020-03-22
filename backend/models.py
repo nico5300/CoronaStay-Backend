0f3344152f4a3d75736e12362344e8c3f2259e31
Butterkeks-x86 <>
2020-03-22 15:42:59 +0100

    Add db relationships

--- backend/models.py
+++ backend/models.py
@@ -1,17 +1,19 @@
 from backend import db
 
 class User(db.Model):
-    username = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, primary_key=True)
     api_key = db.Column(db.String, unique=True)
-    stories = #TODO
+    stories = db.Relationship('Story', backref='user')
 
 
 class Story(db.Model):
-    story_id = db.Column(db.String, primary_key=True)
-    creator = #TODO
+    _id = db.Column(db.String, primary_key=True)
+    user_name = db.Column(db.String, db.ForeignKey('user.name'))
     title = db.Column(db.String)
+    panels = db.Relationship('Panel', backref='story')
 
 class Panel(db.Model):
     panel_id = db.Column(db.Integer, autoincrement=True)
+    story_id = db.Column(db.Integer, db.ForeignKey('story._id'))
     file_name = db.Column(db.String, unique=True)
 
